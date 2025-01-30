868b3555ccf3635b8683ae136cb1def1c36942cc
ezy4me <mrronal@mail.ru>
2025-01-30 12:08:21 +0300

    add contollers and services to all modules

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
+import { EmployeeController } from './employee.controller';
 
-@Module({})
+@Module({
+  providers: [EmployeeService],
+  controllers: [EmployeeController]
+})
 export class EmployeeModule {}
