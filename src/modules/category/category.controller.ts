c3071cfd11da624a6d7a3873876a909cb16f81ab
ezy4me <mrronal@mail.ru>
2025-02-06 08:17:11 +0300

    added: category crud operations

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Param,
+  Body,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { Category } from '@prisma/client';
+import { CategoryDto } from './dto';
+import { Public } from '@common/decorators';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('category')
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Public()
+  @Get()
+  async getAllCategorys(): Promise<Category[]> {
+    return this.categoryService.getAllCategorys();
+  }
+
+  @Public()
+  @Get(':categoryId')
+  async getOneCategoryById(
+    @Param('categoryId', ParseIntPipe) categoryId: number,
+  ): Promise<Category | null> {
+    return this.categoryService.getOneCategoryById(categoryId);
+  }
+
+  @Post()
+  async createCategory(@Body() categoryDto: CategoryDto): Promise<Category> {
+    return this.categoryService.createCategory(categoryDto);
+  }
+
+  @Put(':categoryId')
+  async updateCategory(
+    @Param('categoryId', ParseIntPipe) categoryId: number,
+    @Body() categoryDto: CategoryDto,
+  ): Promise<Category> {
+    return this.categoryService.updateCategory(categoryId, categoryDto);
+  }
+
+  @Delete(':categoryId')
+  async deleteCategory(
+    @Param('categoryId', ParseIntPipe) categoryId: number,
+  ): Promise<Category | null> {
+    return this.categoryService.deleteCategory(categoryId);
+  }
+}
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '@database/database.service';
+import { Category } from '@prisma/client';
+import { CategoryDto } from './dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async getAllCategorys(): Promise<Category[]> {
+    return this.databaseService.category.findMany();
+  }
+
+  async getOneCategoryById(categoryId: number): Promise<Category | null> {
+    return this.databaseService.category.findUnique({
+      where: { id: categoryId },
+    });
+  }
+
+  async createCategory(dto: CategoryDto): Promise<Category> {
+    return this.databaseService.category.create({
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async updateCategory(
+    categoryId: number,
+    dto: CategoryDto,
+  ): Promise<Category> {
+    return this.databaseService.category.update({
+      where: { id: categoryId },
+      data: dto,
+    });
+  }
+
+  async deleteCategory(categoryId: number): Promise<Category | null> {
+    return this.databaseService.category.delete({
+      where: { id: categoryId },
+    });
+  }
+}
