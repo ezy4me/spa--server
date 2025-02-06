ef73a37b905fed665cb606a55bad7aa192d28b65
ezy4me <mrronal@mail.ru>
2025-02-06 08:23:07 +0300

    added: location module crud operations

--- src/modules/location/location.controller.ts
+++ src/modules/location/location.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Param,
+  Body,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
+import { LocationService } from './location.service';
+import { Location } from '@prisma/client';
+import { LocationDto } from './dto';
+import { Public } from '@common/decorators';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('location')
 @Controller('location')
-export class LocationController {}
+export class LocationController {
+  constructor(private readonly locationService: LocationService) {}
+
+  @Public()
+  @Get()
+  async getAllLocations(): Promise<Location[]> {
+    return this.locationService.getAllLocations();
+  }
+
+  @Public()
+  @Get(':locationId')
+  async getOneLocationById(
+    @Param('locationId', ParseIntPipe) locationId: number,
+  ): Promise<Location | null> {
+    return this.locationService.getOneLocationById(locationId);
+  }
+
+  @Post()
+  async createLocation(@Body() locationDto: LocationDto): Promise<Location> {
+    return this.locationService.createLocation(locationDto);
+  }
+
+  @Put(':locationId')
+  async updateLocation(
+    @Param('locationId', ParseIntPipe) locationId: number,
+    @Body() locationDto: LocationDto,
+  ): Promise<Location> {
+    return this.locationService.updateLocation(locationId, locationDto);
+  }
+
+  @Delete(':locationId')
+  async deleteLocation(
+    @Param('locationId', ParseIntPipe) locationId: number,
+  ): Promise<Location | null> {
+    return this.locationService.deleteLocation(locationId);
+  }
+}
--- src/modules/location/location.service.ts
+++ src/modules/location/location.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '@database/database.service';
+import { Location } from '@prisma/client';
+import { LocationDto } from './dto';
 
 @Injectable()
-export class LocationService {}
+export class LocationService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async getAllLocations(): Promise<Location[]> {
+    return this.databaseService.location.findMany();
+  }
+
+  async getOneLocationById(locationId: number): Promise<Location | null> {
+    return this.databaseService.location.findUnique({
+      where: { id: locationId },
+    });
+  }
+
+  async createLocation(dto: LocationDto): Promise<Location> {
+    return this.databaseService.location.create({
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async updateLocation(
+    locationId: number,
+    dto: LocationDto,
+  ): Promise<Location> {
+    return this.databaseService.location.update({
+      where: { id: locationId },
+      data: dto,
+    });
+  }
+
+  async deleteLocation(locationId: number): Promise<Location | null> {
+    return this.databaseService.location.delete({
+      where: { id: locationId },
+    });
+  }
+}
